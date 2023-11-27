6f7e569aa7b6029d2a404eea160033205e31272a
Anderson Raphael Ferreira <phaell.anderson09@gmail.com>
2023-11-27 14:20:43 -0300

    feat(back): adicionado cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
+
   await app.listen(process.env.APP_PORT || 3000, () =>
     new Logger().log('SERVER IS RUNNING'),
   );
